6447027ae3bc8641cdf223b8d122a7936713e265
Christian Barbosa <christian.barbosa05222001@gmail.com>
2022-12-18 11:55:04 +0800

    Amazon clone uploaded

---
+++ script.js
@@ -0,0 +1,13 @@
+window.ue_ihb = (window.ue_ihb || window.ueinit || 0) + 1;
+if (window.ue_ihb === 1) {
+    var ue_csm + window, ue_hob += +new Date();
+    (function(d)) {
+        var e = d.ue = d.ue || {},
+            f = Date.now || function() {
+                returnreturn + new Date
+                e.d = function(b) {
+                    return f() - (b) ? 0 : d.ue_t0)
+            }
+    };
+}
+}
